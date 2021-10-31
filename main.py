2fc85c0fd5f1994a83397b0e5d9c5f32dbed2e5d
Eder Lima <devilsouls666@gmail.com>
2021-10-30 21:00:31 -0300

    fix: add rel to github link and minor changes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import logging
 from flask import Flask
 from rich.logging import RichHandler
-from sql_viewer.config import APPLICATION_PORT
 from sql_viewer.controllers.table_controller import table as table_controller
 from sql_viewer.controllers.api_controller import api as api_controller
 
@@ -23,4 +22,4 @@ app.register_blueprint(table_controller)
 app.register_blueprint(api_controller)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=APPLICATION_PORT)
+    app.run(debug=True, port=8081)
--- sql_viewer/config/__init__.py
+++ sql_viewer/config/__init__.py
@@ -4,4 +4,3 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_FILENAME = environ.get("DATABASE_FILENAME")
-APPLICATION_PORT = environ.get("PORT", 8081)
